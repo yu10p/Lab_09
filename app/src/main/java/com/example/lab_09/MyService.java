10b9d36460dcfcdcf42f2e25b49f1b50b1a09939
yu10p <eason31003@gmail.com>
2022-12-09 10:00:30 +0800

    Lab_09

---
+++ app/src/main/java/com/example/lab_09/MyService.java
@@ -0,0 +1,61 @@
+package com.example.lab_09;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+
+public class MyService extends Service {
+
+    static Boolean flag = false;
+
+    private int h=0, m=0, s=0;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        flag = intent.getBooleanExtra("flag", false);
+
+        //使用Thread來計算秒數
+        new Thread(new Runnable(){
+            @Override
+            public void run(){
+                while(flag){
+                    try{
+                        //使用Thread來計算秒數，延遲1秒
+                        Thread.sleep(1000);
+                    }catch(InterruptedException e){
+                        e.printStackTrace();
+                    }
+
+                    s++;
+
+                    if(s>=60){
+                        s=0;
+                        m++;
+                        if(m>=60){
+                            m=0;
+                            h++;
+                        }
+                    }
+                    Intent i = new Intent("MyMessage");
+
+                    Bundle bundle = new Bundle();
+                    bundle.putInt("H", h);
+                    bundle.putInt("M", m);
+                    bundle.putInt("S", s);
+
+                    i.putExtras(bundle);
+
+                    sendBroadcast(i);
+                }
+            }
+        }).start();
+        return START_STICKY;
+    }
+}
